815418a8e4540352d36de720835c6811abcf481d
Edgar Marquez <18132492+thisEdgarMarquez@users.noreply.github.com>
2017-12-22 01:17:57 -0400

    Update validar_rif.php

--- validar_rif.php
+++ validar_rif.php
@@ -5,15 +5,12 @@
 	if(!isset($_SESSION['captchaId'])) header("Location: index.php");
 
     if(!isset($_POST["rif"]) && empty($_POST["rif"])) header("Location: index.php");
-    /*=============================================================================*/
 
-	require_once "seniat.ini.php";
-	
-	$captcha = $_POST["captcha"];
-	$rif     = $_POST["rif"];
+	require_once "Seniat.php";
+	$seniat = new Seniat();
     $dataSeniat = array();
 
-    $dataSeniat['status'] = $seniat->Consultar($captcha, $rif, $_SESSION['captchaId']);
+    $dataSeniat['status'] = $seniat->Consultar($_POST["captcha"];, $_POST["rif"], $_SESSION['captchaId']);
     unlink('tmp/'.$_SESSION['captchaId'].'.jpg');
     unlink('tmp/'.$_SESSION['captchaId'].'.txt');
     session_destroy();
